c9b271cdcaa9b09df81619310d4f9aeb281a7c78
student1232354 <tarasromanenko33@gmail.com>
2026-07-05 15:52:14 +0300

    first update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,6 +25,8 @@ const tree = document.querySelector('#tree');
 function createTree(element, data) {
 
   if (!data || typeof data !== 'object') return;
+  if (!data || !Object.keys(data).length) return;
+
   const ul = document.createElement('ul');
 
   for (const objs in data) {
